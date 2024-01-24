522a46e8e4f8e9cca6c4f64da86095583cfaa9bd
KarlaCianfa <karla-cianfa@hotmail.com>
2024-01-23 21:16:59 -0300

    Inicio de nova tela de cadastro de contato.

---
+++ lib/NovoCadastroScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NovoCadastroScreen extends StatefulWidget {
+  const NovoCadastroScreen({super.key});
+
+  @override
+  State<NovoCadastroScreen> createState() => _NovoCadastroScreenState();
+}
+
+class _NovoCadastroScreenState extends State<NovoCadastroScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // Montando o esqueleto da nova página
+    return Scaffold();
+  }
+}
